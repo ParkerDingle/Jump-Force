873c8a6cdd2cbb460a22446d7d12dc9c71f7d2a4
Parker Dingle <parkerad2023@gmail.com>
2025-05-16 12:11:56 -0400

    Small Changes to Player & Object
    Added objects with spawn manager, Gravity & physics, Rigid Bodies, Stopped player from being able to double jump, Obstacle and background now fly to the left.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,19 +3,32 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 private Rigidbody playerRB;
+
+public float jumpForce = 10;
+
+public float gravityModifier;
+
+public bool isOnGround = true;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         playerRB = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
         {
             // impulse force mode immidiently applies force
-            playerRB.AddForce(Vector3.up * 10, ForceMode.Impulse);
+            playerRB.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
         } 
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        isOnGround = true;
     }
 }
